eb07817b2fbf73336a1b6f28e3588a8bedbc1f8d
aorri2 <aorri96@naver.com>
2023-08-01 14:19:42 +0900

    feat : String substring 학습 테스트 작성
    - substring의 endIndex는 exclusive이기에, 마지막 인덱스 번호 + 1 값으로 해줘야함

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -14,8 +14,24 @@ public class StringTest {
 
     @Test
     @DisplayName("'1,2'를 split 했을 때 1과 2로 나와야 한다.")
-    void givenNumber_whenSplitString_thenReturnSplittedNumber() {
+    void givenNumber_whenSplitString_thenReturnSplittedNumbers() {
         String[] actual = "1,2".split(",");
         assertThat(actual).containsExactly("1","2");
     }
+
+    @Test
+    @DisplayName("'1,'를 split 했을 때 1로 나와야 한다.")
+    void givenNumber_whenSplitString_thenReturnSplittedNumber() {
+        String[] actual = "1,".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("('1,2)'를 subString 했을 때 ()를 제거하고 '1,2'를 반환해야 한다.")
+    void givenNumberInParentheses_whensubstring_thenReturnNumber() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+
 }
