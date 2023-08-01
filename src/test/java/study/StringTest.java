63ccc9303cd995d60420471b162a7221f387ec5a
aorri2 <aorri96@naver.com>
2023-08-01 14:14:56 +0900

    feat : String split 학습 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,21 @@
 package study;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("'1,2'를 split 했을 때 1과 2로 나와야 한다.")
+    void givenNumber_whenSplitString_thenReturnSplittedNumber() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1","2");
     }
 }
