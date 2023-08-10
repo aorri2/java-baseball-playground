b7fed7d5f449e4ea9f165627ad989162f98bea9d
aorri2 <aorri96@naver.com>
2023-08-10 19:25:03 +0900

    refactor : 문자열 계산기 3차 실습
    - Map 자료구조를 사용해, operator를 찾아오는 속도 향상
    - calculatePartial()삭제
    - 불필요한 field 삭제

--- src/main/java/study/Operator.java
+++ src/main/java/study/Operator.java
@@ -1,6 +1,10 @@
 package study;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum Operator {
 	PLUS("+",(firstOperand,secondOperand) -> firstOperand + secondOperand),
@@ -9,6 +13,8 @@ public enum Operator {
 	DIVIDE("/",(firstOperand,secondOperand) -> firstOperand / secondOperand),
 	;
 
+	private static final Map<String,Operator> operators = Collections.unmodifiableMap(
+		Stream.of(values()).collect(Collectors.toMap(operator->operator.getSymbol(),operator -> operator)));
 	private final String symbol;
 	private final BinaryOperator<Integer> operation;
 	Operator(String symbol, BinaryOperator<Integer> operation) {
@@ -18,6 +24,15 @@ public enum Operator {
 
 	public String getSymbol() {
 		return symbol;
+	}
+
+	public static Operator findOperator(String symbol) {
+		Operator operator = operators.get(symbol);
+
+		if (operator == null) {
+			throw new IllegalArgumentException();
+		}
+		return operator;
 	}
 
 	public int operate(int firstOperand, int secondOperand) {
--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -5,39 +5,25 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+	private static final Pattern operatorRegExp = Pattern.compile("^[0-9]*$");
+
 	private Formula formula;
-	private int result = 0;
-	private Operator currentOperator = Operator.PLUS;
 
 	public StringCalculator(Formula formula) {
 		this.formula = formula;
 	}
 
 	public int calculateFormula() {
+		int result = 0;
+		Operator currentOperator = Operator.PLUS;
 		for (String input : formula.split(" ")) {
-			calculatePartial(input);
+			if (operatorRegExp.matcher(input).find()) {
+				result = currentOperator.operate(result, Integer.parseInt(input));
+				continue;
+			}
+			currentOperator = Operator.findOperator(input);
 		}
 		return result;
 	}
 
-	private void calculatePartial(String input) {
-		String regulaExpression = "^[0-9]*$";
-
-		if (Pattern.matches(regulaExpression,input)) {
-			result = currentOperator.operate(result, Integer.parseInt(input));
-			return;
-		}
-
-		for(Operator operator : Operator.values()){
-			if(operator.getSymbol().equals(input)){
-				currentOperator = operator;
-				return;
-			}
-		}
-
-		currentOperator = Arrays.stream(Operator.values())
-			.filter(operator -> operator.getSymbol().equals(input))
-			.findFirst()
-			.orElseThrow(IllegalArgumentException::new);
-	}
 }
